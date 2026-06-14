449d373451c01ad7d185bf6086b22c39e8957102
vladimirpavlov812 <vladimir.pavlov812@gmail.com>
2026-06-14 15:25:29 +0200

    update site

--- src/content/site.ts
+++ src/content/site.ts
@@ -29,7 +29,7 @@ export const site = {
 
   cv: {
     label: "Скачать CV",
-    href: "https://disk.yandex.ru/i/RdfQ2M8Igaa9JA",
+    href: "https://disk.yandex.ru/i/8F50UJu3jdeusw",
   },
 
   links: {
